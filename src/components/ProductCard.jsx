9efe31cebf54e97019d627acc88201c7398b6147
dev-kishor138 <dev.kishor138@gmail.com>
2024-12-25 15:15:06 +0600

    kishor

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,22 +1,80 @@
-import image from '../assets/img/product.jpg';
+import { Icon } from '@iconify/react/dist/iconify.js';
+import defaultImage from '../assets/img/product.jpg';
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+    const { brand, price, title, discountPercentage, category, thumbnail } = product;
+
+    let discountAmount = 0;
+    let finalPrice = 0;
+
+    if (discountPercentage ?? discountPercentage > 0) {
+        discountAmount = Math.ceil((discountPercentage * price) / 100);
+        finalPrice = (price - discountAmount).toFixed(2);
+    }
+
     return (
-        <div className="bg-slate-200 rounded-md p-1 relative">
-            <div className='absolute top-5 -left-2 bg-gradient-to-b from-[#FFA03B] to-[#F27D00] text-md px-3 text-white before:content-[""] before:w-4 before:h-4 before:bg-gradient-to-b before:from-[#FFA03B] before:to-[#F27D00] before:absolute before:-bottom-[7px] before:left-1 rounded-ss-lg before:-z-10 before:rotate-[130deg] after:content-[""] after:w-0 after:h-0 after:border-l-[10px] after:border-r-[10px] after:border-b-[10px] after:border-l-transparent after:border-r-transparent after:border-b-[#F7B166] after:absolute after:top-0 after:-right-1'>
+        <div className="rounded-md p-1 relative group hover:shadow-xl hover:bg-white w-full max-w-[300px] bg-slate-50">
+            <div className='relative w-full'>
+            <Icon
+            icon="solar:heart-linear"
+            width="24"
+            height="24"
+            className="absolute top-5 right-5 z-10 text-green-500 transition-all hover:opacity-0"
+        />
 
-                <p>-৳200</p>
+        {/* Hover Icon */}
+        <Icon
+            icon="solar:heart-bold"
+            width="24"
+            height="24"
+            className="absolute top-5 right-5 z-10 text-red-500 transition-all opacity-0 hover:opacity-100"
+        />
+                {
+                    discountPercentage !== null && discountPercentage !== undefined && discountPercentage > 0 ? (
+                        <>
+                            <div className='absolute top-[35px] -left-[4px] w-4 h-4 bg-gradient-to-b from-[#FFA03B] to-[#F27D00] rotate-[130deg] -z-10'>
+                            </div>
+                            <div className='absolute top-5 -left-2 bg-gradient-to-b from-[#FFA03B] to-[#F27D00] text-md h-6 w-[80px] px-3 text-white rounded-ss-lg z-20 text-center'>
+                                <p>-{" "}${discountAmount}</p>
+                            </div>
+                            <div
+                                className="absolute top-5 left-[60px] h-6 w-0 border-l-[12px] border-l-transparent border-r-[12px] border-r-transparent border-b-[24px] border-b-[#F27D00] before:absolute before:top-0 before:-left-3 before:h-6 before:w-0 before:border-l-[12px] before:border-l-transparent before:border-r-[12px] before:border-r-transparent before:border-b-[24px] before:border-b-[#F7B166] before:rotate-180 rounded-sm before:rounded-sm z-10"
+                            >
+                            </div>
+                        </>
+                    ) : null
+                }
             </div>
 
-            <div className='w-full h-[200px]  rounded-md overflow-hidden'>
-                <img src={image} alt="Product Image" className='h-full w-full object-cover' />
-            </div>
-            <div className='px-5'>
-                <h6>Fabrilife</h6>
-                <h4>Fabrilife Mens Premium Designer Edition T Shirt</h4>
-                <p>৳2500 <span>৳2700</span></p>
-            </div>
 
+            <div className='w-full h-[200px]  rounded-md overflow-hidden relative'>
+                <img src={thumbnail || defaultImage} alt="Product Image" className='h-full w-full object-cover' />
+                <div className='absolute bottom-5 left-0 w-full z-10 px-5 grid gap-2 translate-x-full group-hover:-translate-x-0 transition-transform duration-300'>
+                    <button className='flex justify-center py-1 w-full bg-opacity-30 bg-white text-green-500 rounded-md  border border-green-500 border-opacity-30 hover:bg-[#03A629] hover:text-white transition-all'>
+                        <Icon icon="ic:outline-shopping-cart" width="24" height="24" />Add To Cart</button>
+                    <button className='flex justify-center py-1 w-full bg-opacity-30 bg-white text-green-500 rounded-md  border border-green-500 border-opacity-30 hover:bg-[#03A629] hover:text-white transition-all'>
+                        <Icon icon="ic:outline-remove-red-eye" width="24" height="24" />Quick View</button>
+                </div>
+            </div>
+            <div className='px-5 py-3'>
+                <h6 className='text-[#5A6573] font-sm font-normal capitalize'>{brand ?? category ?? ""}</h6>
+                <h4 className='text-[#1A2B3D] font-bold text-lg mb-2'>{title ?? ""}</h4>
+
+                {
+                    discountPercentage !== null && discountPercentage !== undefined && discountPercentage > 0 ? (
+                        <p className='text-[#1882FF] font-bold text-2xl'>
+                            ${finalPrice ?? 0} {" "}
+                            <span className='text-[#77818C] text-lg font-normal'>
+                                <del>${price}</del>
+                            </span>
+                        </p>
+                    ) : (
+                        <p className='text-[#1882FF] font-bold text-2xl'>
+                            ${price.toFixed(2) ?? 0}
+                        </p>
+                    )
+                }
+            </div>
         </div>
     );
 };
--- src/pages/all-products/AllProducts.jsx
+++ src/pages/all-products/AllProducts.jsx
@@ -3,17 +3,16 @@ import { useGetAllProductsQuery } from "../../redux/features/api/productsApiSlic
 
 const AllProducts = () => {
 
-    const { data,error, isLoading } = useGetAllProductsQuery();
+    const { data, error, isLoading } = useGetAllProductsQuery();
     console.log(data);
 
-    if(isLoading) return <p>Loading</p>;
-    if(error) return <p>{error}</p>
+    if (isLoading) return <p>Loading</p>;
+    if (error) return <p>{error}</p>
     return (
-        <div className="grid grid-cols md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mx-10 my-5">
-            {/* {
-                data.products.map(product => <p key={product.id}>{product.title ?? ""}</p>)
-            } */}
-            <ProductCard/>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mx-auto my-5 justify-items-center">
+            {
+                data.products.map(product => <ProductCard key={product.id} product={product} />)
+            }
         </div>
     );
 };
