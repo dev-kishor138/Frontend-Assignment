85452b0d9808d1f1e3bfb440325984a68f48b86b
dev-kishor138 <dev.kishor138@gmail.com>
2024-12-29 22:51:20 +0600

    complete

--- src/components/CartInfo.jsx
+++ src/components/CartInfo.jsx
@@ -1,11 +1,28 @@
 
 import { Icon } from "@iconify/react/dist/iconify.js";
 import useCart from "../hooks/useCart";
+import Swal from "sweetalert2";
 
 const CartInfo = ({ product }) => {
     const { price, title, thumbnail, quantity } = product;
     const { addProduct, removeProduct, updateByOne } = useCart();
 
+    const handleRemove = () => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "Do you really want to remove this item from the cart?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, remove it!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                removeProduct(product);
+                Swal.fire("Removed!", "The product has been removed.", "success");
+            }
+        });
+    };
     return (
         <>
             <div>
@@ -30,7 +47,7 @@ const CartInfo = ({ product }) => {
                 {(price * quantity).toFixed(2)}
             </div>
             <div className="flex justify-center items-center">
-                <button className="bg-red-400 text-white px-2 py-1 rounded-sm" onClick={() => removeProduct(product)}>
+                <button className="bg-red-400 text-white px-2 py-1 rounded-sm" onClick={handleRemove}>
                     <Icon icon="solar:trash-bin-trash-linear" width="24" height="24" />
                 </button>
             </div>
